f2ad82a4cfc515487e52b2871ceb482795e15b88
logoff <juliocarlos.barrera@i2cat.net>
2013-11-14 16:08:51 +0100

    WorkingMessageQueue with his own logger.

--- src/main/java/net/i2cat/netconf/messageQueue/WorkingMessageQueue.java
+++ src/main/java/net/i2cat/netconf/messageQueue/WorkingMessageQueue.java
@@ -2,6 +2,9 @@ package net.i2cat.netconf.messageQueue;
 
 import net.i2cat.netconf.rpc.RPCElement;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Working Message Queue extending {@link MessageQueue}
  * 
@@ -9,6 +12,8 @@ import net.i2cat.netconf.rpc.RPCElement;
  * 
  */
 public class WorkingMessageQueue extends MessageQueue {
+
+	static Log	log	= LogFactory.getLog(WorkingMessageQueue.class);
 
 	@Override
 	public RPCElement consume() {
